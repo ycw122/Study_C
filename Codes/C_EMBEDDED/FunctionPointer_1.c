d3a3d6bea88bb27c88d1003af8a114bbde0c6f82
ycw122 <1731946954@qq.com>
2026-07-02 23:30:42 +0800

    add codes for embedded C

---
+++ Codes/C_EMBEDDED/FunctionPointer_1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+
+
+int add(int a, int b){return a+b;}
+int subtract(int a, int b){return a-b;}
+int multiplication(int a, int b){return a*b;}
+int division(int a, int b){return a/b;}
+
+int (*p)(int a, int b);
+
+int main(){
+    int m,n;
+    char c;
+    printf("please input an operator,like + _ * / \n");
+    scanf("%c",&c);
+     if (c=='+')
+    {
+        p=add;
+        /* code */
+    }else if (c=='-')
+    {
+        p=subtract;
+        /* code */
+    }else if (c=='*')
+    {
+        p=multiplication;
+        /* code */
+    }else if (c=='/')
+    {
+        p=division;
+        /* code */
+    }else{
+        printf("please input an corrcet operator!\n");
+        return 0;
+    }
+    printf("please input two numbers\n");
+    scanf("%d %d",&m,&n);
+    if (p==division&&m<=0)
+    {
+        printf("the first number must be an positive number!\n");
+        return 0;
+    }
+    printf("result = %d\n",p(m,n));
+    return 0;
+    
+}
